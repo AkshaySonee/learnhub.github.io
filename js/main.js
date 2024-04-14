267907644a21f1ef642eeae5e2e7b3fe67307321
Akshay Kumar <theakshaykumar13@gmail.com>
2024-04-14 17:50:55 +0530

    Learn Hub

---
+++ js/main.js
@@ -0,0 +1,30 @@
+(function ($) {
+    "use strict";    
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.sticky-top').css('top', '0px');
+        } else {
+            $('.sticky-top').css('top', '-100px');
+        }
+    });
+    
+    // Header carousel
+    $(".header-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        items: 1,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ]
+    });
+})(jQuery);
